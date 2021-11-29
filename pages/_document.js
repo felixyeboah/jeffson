f682e337e89818f27b89e670e1dc4671b62e6323
Felix Nana Yaw Yeboah <87368189+felixyeboah@users.noreply.github.com>
2021-11-29 12:15:42 +0000

    updated projects page

--- pages/_document.js
+++ pages/_document.js
@@ -8,9 +8,14 @@ class MyDocument extends Document {
           <link href="/favicon.ico" rel="shortcut icon" />
           <link href="/static/favicons/site.webmanifest" rel="manifest" />
           <link href="/fonts/fonts.css" rel="stylesheet" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;700;800;900&family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;200;300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
           <link href="/favicon.ico" rel="apple-touch-icon" sizes="180x180" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        sans: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans],
         serif: ["Playfair Display", ...defaultTheme.fontFamily.serif],
         uses: ["OperatorMono", ...defaultTheme.fontFamily.serif],
         blog: ["Larsseit", ...defaultTheme.fontFamily.sans],
@@ -39,7 +39,7 @@ module.exports = {
         booksBg: "rgb(234, 233, 228)",
         booksCardBg: "rgba(255, 255, 255, 0.4)",
         blogBg: "rgb(227, 230, 236)",
-        curated: '#2D4298'
+        curated: "#2D4298",
       },
       fontSize: {
         "10xl": "10rem",
